d08705bc4e7d164e5cb9ef59192db78685c74360
Amir Hossein Mansoori <39344547+mans82@users.noreply.github.com>
2024-04-04 00:01:32 +0330

    Feature: Add yaml testcases (#5)
    * Add yaml test cases
    
    * Add support for using YAML test cases
    
    * Temporarily fix testcase yaml path

--- pkg/lookup/lookup_test.go
+++ pkg/lookup/lookup_test.go
@@ -1,200 +1,50 @@
 package lookup
 
 import (
+	"os"
 	"testing"
 
 	"github.com/mans82/ldoce-cli/pkg/parser"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 func TestLookUpDefault(t *testing.T) {
+
 	t.Parallel()
 
-	tests := []struct {
-		word  string
-		entry parser.Entry
-	}{
-		{
-			word: "bucket",
-			entry: parser.Entry{
-				SubEntries: []parser.SubEntry{
-					{
-						HyphenatedText: "buck‧et",
-						IPA:            "/ˈbʌkɪt/",
-						Type:           "noun",
-						GrammerNotes:   "countable",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"an open container with a handle, used for carrying and holding things, especially liquids",
-							"the quantity of liquid that a bucket can hold",
-							"a part of a machine shaped like a large bucket and used for moving earth, water etc",
-							"a large amount of something",
-						},
-					},
-					{
-						HyphenatedText: "bucket",
-						IPA:            "",
-						Type:           "verb",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-					},
-				},
-			},
-		},
-		{
-			word: "give",
-			entry: parser.Entry{
-				SubEntries: []parser.SubEntry{
-					{
-						HyphenatedText: "give",
-						IPA:            "/ɡɪv/",
-						Type:           "verb",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"to let someone have something as a present, or to provide something for someone",
-							"to put something in someone’s hand",
-							"to allow or make it possible for someone to do something",
-							"to tell someone information or details about something, or to tell someone what they should do",
-							"to do something by making a movement with your hand, face, body etc",
-							"to make a speech, perform a piece of music etc for a group of people",
-							"to make someone have a feeling",
-							"to make someone have problems",
-							"to infect someone with the same illness that you have",
-							"to organize a social event such as a party",
-							"to tell someone to do a job or piece of work",
-							"to make someone or something have a particular quality",
-							"to pay a particular amount of money for something",
-							"to behave towards someone in a way that shows you have a particular attitude or feeling towards them",
-							"to officially say that someone must have a particular punishment",
-							"to be willing to change what you think or do according to what else happens",
-							"to state what your official decision or judgment is, for example in a game",
-							"to decide that someone should have a particular score or mark for something that they have done",
-							"if a material gives, it bends or stretches when you put pressure on it",
-							"if something gives, it breaks or moves away suddenly because of weight or pressure on it",
-							"if a woman gives herself to a man, she has sex with him",
-						},
-					},
-					{
-						HyphenatedText: "give",
-						IPA:            "",
-						Type:           "noun",
-						GrammerNotes:   "uncountable",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"the ability of a material or substance to bend or stretch when put under pressure",
-						},
-					},
-				},
-			},
-		},
-		{
-			word: "nerv",
-			entry: parser.Entry{
-				SubEntries: []parser.SubEntry{
-					{
-						HyphenatedText: "nerve",
-						IPA:            "/nɜːv $ nɜːrv/",
-						Type:           "noun",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"nerves are parts inside your body which look like threads and carry messages between the brain and other parts of the body",
-							"courage and confidence in a dangerous, difficult, or frightening situation",
-							"if you say someone has a nerve, you mean that they have done something unsuitable or impolite, without seeming to be embarrassed about behaving in this way",
-						},
-					},
-					{
-						HyphenatedText: "nerve",
-						IPA:            "",
-						Type:           "verb",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-					},
-				},
-			},
-		},
-		{
-			word: "co",
-			entry: parser.Entry{
-				SubEntries: []parser.SubEntry{
-					{
-						HyphenatedText: "co-",
-						IPA:            "/kəʊ $ koʊ/",
-						Type:           "prefix",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"together with",
-							"doing something with someone else as an equal or with less responsibility",
-						},
-					},
-					{
-						HyphenatedText: "Co.",
-						IPA:            "/kəʊ $ koʊ/",
-						Type:           "",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"the abbreviation of company",
-							"the written abbreviation of county",
-						},
-					},
-					{
-						HyphenatedText: "CO",
-						IPA:            "",
-						Type:           "",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"the written abbreviation of Colorado",
-						},
-					},
-					{
-						HyphenatedText: "C.O.",
-						IPA:            "/ˌsiː ˈəʊ◂ $ -ˈoʊ◂/",
-						Type:           "noun",
-						GrammerNotes:   "countable",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"an officer who is in charge of a military unit",
-						},
-					},
-					{
-						HyphenatedText: "Co",
-						IPA:            "",
-						Type:           "",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"written abbreviation for Company",
-						},
-					},
-					{
-						HyphenatedText: "co",
-						IPA:            "/kəʊkoʊ/",
-						Type:           "prefix",
-						GrammerNotes:   "",
-						ExtraInfo:      "",
-						Definitions: []string{
-							"added to the front of a noun to show that someone does a job with someone else",
-						},
-					},
-				},
-			},
-		},
+	allEntries, err := parser.GetAllTestEntries("../parser/testdata/all-entries.yaml") // FIXME: Find a better way to organize test data
+
+	if err != nil {
+		t.Errorf("error getting test entries: %v", err)
+	}
+
+	testsYamlFile, err := os.ReadFile("./testdata/tests.yaml")
+	if err != nil {
+		t.Errorf("error reading test data: %v", err)
+	}
+
+	var tests []struct {
+		Query        string `yaml:"query"`
+		ExpectedWord string `yaml:"expectedWord"`
+	} = nil
+
+	err = yaml.Unmarshal(testsYamlFile, &tests)
+
+	if err != nil {
+		t.Errorf("error unmarshalling test data: %v", err)
 	}
 
 	for _, test := range tests {
 		test := test
-		t.Run(test.word, func(t *testing.T) {
+		t.Run(test.Query, func(t *testing.T) {
 			t.Parallel()
-			entry, err := LookupDefault(test.word)
+			entry, err := LookupDefault(test.Query)
 			if err != nil {
 				t.Errorf("Error looking up: %v", err)
 			}
 
-			assert.Equal(t, test.entry, *entry)
+			assert.Equal(t, allEntries[test.ExpectedWord], *entry)
 		})
 	}
 }
--- pkg/parser/entry_test.go
+++ pkg/parser/entry_test.go
@@ -14,197 +14,21 @@ func TestParseEntry(t *testing.T) {
 
 	client := &http.Client{}
 
-	tests := []struct {
-		url        string
-		subEntries []SubEntry
-	}{
-		{
-			url: "https://www.ldoceonline.com/dictionary/bucket",
-			subEntries: []SubEntry{
-				{
-					HyphenatedText: "buck‧et",
-					IPA:            "/ˈbʌkɪt/",
-					Type:           "noun",
-					GrammerNotes:   "countable",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"an open container with a handle, used for carrying and holding things, especially liquids",
-						"the quantity of liquid that a bucket can hold",
-						"a part of a machine shaped like a large bucket and used for moving earth, water etc",
-						"a large amount of something",
-					},
-				},
-				{
-					HyphenatedText: "bucket",
-					IPA:            "",
-					Type:           "verb",
-					GrammerNotes:   "",
-					ExtraInfo:      "",
-				},
-			},
-		},
-		{
-			url: "https://www.ldoceonline.com/dictionary/take",
-			subEntries: []SubEntry{
-				{
-					HyphenatedText: "take",
-					IPA:            "/teɪk/",
-					Type:           "verb",
-					GrammerNotes:   "",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"to move or go with someone or something from one place to another",
-						"used with a noun instead of using a verb to describe an action. For example, if you take a walk, you walk somewhere",
-						"to remove something from a place",
-						"if something takes a particular amount of time, money, effort etc, that amount of time etc is needed for it to happen or succeed",
-						"to accept or choose something that is offered, suggested, or given to you",
-						"to get hold of something in your hands",
-						"to use a particular form of transport or a particular road in order to go somewhere",
-						"to study a particular subject in school or college for an examination",
-						"to do an examination or test",
-						"to be the correct or suitable size, type etc for a particular person or thing",
-						"to collect or gather something for a particular purpose",
-						"to react to someone or something or consider them in a particular way",
-						"to have or experience a particular feeling",
-						"to get possession or control of something",
-						"to swallow, breathe in, inject etc a drug or medicine",
-						"to make someone or something go to a higher level or position",
-						"to measure the amount, level, rate etc of something",
-						"to make a number smaller by a particular amount",
-						"if a shop, business etc takes a particular amount of money, it receives that amount of money from its customers",
-						"to teach a particular group of students in a school or college",
-						"to write down information",
-						"if a man takes someone, he has sex with them",
-						"if a treatment, dye, drug etc takes, it begins to work successfully",
-					},
-				},
-				{
-					HyphenatedText: "take",
-					IPA:            "",
-					Type:           "noun",
-					GrammerNotes:   "",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"an occasion when a film scene, song, action etc is recorded",
-						"the amount of money earned by a shop or business in a particular period of time",
-					},
-				},
-				{
-					HyphenatedText: "take",
-					IPA:            "/teɪk/",
-					Type:           "noun",
-					GrammerNotes:   "countable usually singular",
-					ExtraInfo:      "informal",
-					Definitions: []string{
-						"the amount of money earned by a business in a particular period of time",
-						"to be willing to do something wrong or illegal in return for money",
-					},
-				},
-				{
-					HyphenatedText: "take",
-					IPA:            "",
-					Type:           "verb",
-					GrammerNotes:   "transitive",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"to subtract one number from another number",
-						"if a business takes or takes in a particular amount of money, it earns that money from selling its goods and services",
-					},
-				},
-			},
-		},
-		{
-			url: "https://www.ldoceonline.com/dictionary/shadow",
-			subEntries: []SubEntry{
-				{
-					HyphenatedText: "shad‧ow",
-					IPA:            "/ˈʃædəʊ $ -doʊ/",
-					Type:           "noun",
-					GrammerNotes:   "",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"the dark shape that someone or something makes on a surface when they are between that surface and the light",
-						"darkness caused by something preventing light from reaching a place",
-						"the bad effect or influence that something has, which makes other things seem less enjoyable, attractive, or impressive",
-					},
-				},
-				{
-					HyphenatedText: "shadow",
-					IPA:            "",
-					Type:           "verb",
-					GrammerNotes:   "transitive",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"to follow someone closely in order to watch what they are doing",
-						"to cover something with a shadow, or make it dark",
-					},
-				},
-				{
-					HyphenatedText: "shadow",
-					IPA:            "",
-					Type:           "adjective",
-					GrammerNotes:   "only before noun",
-					ExtraInfo:      "",
-				},
-				{
-					HyphenatedText: "shad‧ow",
-					IPA:            "/ˈʃædəʊ-doʊ/",
-					Type:           "verb",
-					GrammerNotes:   "transitive",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"to watch someone very closely or work with them in order to learn how they do their job",
-						"to change at the same rate or in the same way as something",
-					},
-				},
-			},
-		},
-		{
-			url: "https://www.ldoceonline.com/dictionary/triumph",
-			subEntries: []SubEntry{
-				{
-					HyphenatedText: "triumph",
-					IPA:            "",
-					Type:           "verb",
-					GrammerNotes:   "intransitive",
-					ExtraInfo:      "formal",
-					Definitions: []string{
-						"to gain a victory or success after a difficult struggle",
-					},
-				},
-				{
-					HyphenatedText: "tri‧umph",
-					IPA:            "/ˈtraɪəmf/",
-					Type:           "noun",
-					GrammerNotes:   "",
-					ExtraInfo:      "",
-					Definitions: []string{
-						"an important victory or success after a difficult struggle",
-						"a feeling of pleasure and satisfaction that you get from victory or success",
-						"a very successful example of something",
-					},
-				},
-				{
-					HyphenatedText: "Triumph",
-					IPA:            "",
-					Type:           "",
-					GrammerNotes:   "",
-					ExtraInfo:      "trademark",
-					Definitions: []string{
-						"a type of motorcycle made by the British company Triumph, which is known for being well-made in a traditional way. The Triumph company also used to make sports cars.",
-					},
-				},
-			},
-		},
+	tests, err := GetAllTestEntries("./testdata/all-entries.yaml")
+
+	if err != nil {
+		t.Errorf("Error getting test entries: %v", err)
 	}
 
-	for _, test := range tests {
+	for word, test := range tests {
 		test := test
-		t.Run(test.url, func(t *testing.T) {
+		test_url := "https://www.ldoceonline.com/dictionary/" + word
+
+		t.Run(word, func(t *testing.T) {
 
 			t.Parallel()
 
-			req, err := http.NewRequest("GET", test.url, nil)
+			req, err := http.NewRequest("GET", test_url, nil)
 			if err != nil {
 				t.Errorf("Error creating request: %v", err)
 			}
@@ -221,7 +45,7 @@ func TestParseEntry(t *testing.T) {
 				t.Errorf("Error parsing entry: %v", err)
 			}
 
-			assert.Equal(t, test.subEntries, entries.SubEntries)
+			assert.Equal(t, test.SubEntries, entries.SubEntries)
 
 		})
 	}
