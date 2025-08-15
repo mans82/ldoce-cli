c593fbaf030396b26f4fb1c4da6b7113304fcdb2
Amir Hossein Mansoori <cando428@gmail.com>
2025-08-15 22:40:07 +0330

    Feature: Add basic argument parsing functionality

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/mans82/ldoce-cli/pkg/formatter"
 	"github.com/mans82/ldoce-cli/pkg/lookup"
 )
 
+
 func main() {
+
+	parseArgs()
 
 	entries, err := lookup.LookupDefault(os.Args[1])
 
@@ -22,4 +27,29 @@ func main() {
 	for _, entry := range entries.Entries {
 		formatter.PrintFormattedEntry(bufferedStdout, entry)
 	}
+}
+
+func usage() {
+	fmt.Printf("Usage: %s [options] <word>\n\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(2)
+
+}
+
+func parseArgs() {
+	flag.Usage = usage
+	helpFlag := flag.Bool("help", false, "show this help message")
+
+	flag.Parse()
+
+	if *helpFlag {
+		usage()
+	}
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
 }
